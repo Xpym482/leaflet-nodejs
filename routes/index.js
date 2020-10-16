7beefc75bee86dd74230d5d0c847063632c06445
Maks <6610257+Xpym482@users.noreply.github.com>
2020-10-16 07:40:52 +0300

    lisatud ulesanne ja reworked db methods

--- routes/index.js
+++ routes/index.js
@@ -17,13 +17,34 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/map', function (req, res){
-  ref = db.ref("/");
-  let docs = {};
-  ref.on("value", function(snapshot) {
-    docs = snapshot.val();
+router.post('/addmarker', function (req, res) {
+  const coord = req.body.coord.split(',');
+  const lat = coord[0];
+  const lng = coord[1];
+  ref = db.ref("/marker");
+  ref.set({
+    lat, lng
   })
+  res.send("marker added");
+})
 
+router.get('/getmarker', function (req, res){
+  ref = db.ref("/marker");
+  ref.once("value")
+      .then(function (data) {
+        res.json(data.val());
+      })
+})
+
+router.get('/map', async function (req, res){
+  ref = db.ref("/layers");
+  const docs = await ref.once("value")
+      .then(function (data){
+        return data.val();
+      })
+      .catch(function (err) {
+        console.log(err);
+      })
   res.render('map', {
     "jmap" : docs,
     lat : 40.78854,
@@ -33,18 +54,26 @@ router.get('/map', function (req, res){
 
 router.get('/mapjson/:name', function (req, res){
   if(req.params.name){
-    ref = db.ref(req.params.name);
-    ref.on("value", function (snapshot){
-      res.json(snapshot.val());
-    })
+    ref = db.ref("/layers/" + req.params.name);
+    ref.once("value")
+        .then(function (data) {
+          res.json(data.val());
+        })
+        .catch(function (err) {
+          console.log(err);
+        })
   }
 })
 
 router.get('/maplayers', function (req, res){
-  ref = db.ref("/");
-  ref.on("value", function(snapshot) {
-    res.json(snapshot.val());
-  })
+  ref = db.ref("/layers");
+  ref.once("value")
+      .then(function (data) {
+        res.json(data.val());
+      })
+      .catch(function (err) {
+        console.log(err);
+      })
 })
 
 module.exports = router;
